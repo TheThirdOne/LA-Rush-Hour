e65eca943577f3921270d75a4ae6524955381b56
Benjamin Landers <benjaminrlanders@gmail.com>
2013-07-11 21:47:14 -0700

    made a useful demo

--- LA-Rush-Hour-core/src/com/benjaminlanders/rushhour/RushHour.java
+++ LA-Rush-Hour-core/src/com/benjaminlanders/rushhour/RushHour.java
@@ -15,24 +15,25 @@ public class RushHour implements ApplicationListener {
 	private SpriteBatch batch;
 	private Texture texture;
 	private Sprite sprite;
+	float w, h;
 	
 	@Override
 	public void create() {		
-		float w = Gdx.graphics.getWidth();
-		float h = Gdx.graphics.getHeight();
+		w = Gdx.graphics.getWidth();
+		h = Gdx.graphics.getHeight();
 		
-		camera = new OrthographicCamera(1, h/w);
+		//camera = new OrthographicCamera(1, h/w);
 		batch = new SpriteBatch();
 		
 		texture = new Texture(Gdx.files.internal("data/libgdx.png"));
-		texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
+		//texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
 		
 		TextureRegion region = new TextureRegion(texture, 0, 0, 512, 275);
 		
-		sprite = new Sprite(region);
-		sprite.setSize(0.9f, 0.9f * sprite.getHeight() / sprite.getWidth());
-		sprite.setOrigin(sprite.getWidth()/2, sprite.getHeight()/2);
-		sprite.setPosition(-sprite.getWidth()/2, -sprite.getHeight()/2);
+		//sprite = new Sprite(region);
+		//sprite.setSize(0.9f, 0.9f * sprite.getHeight() / sprite.getWidth());
+		//sprite.setOrigin(sprite.getWidth()/2, sprite.getHeight()/2);
+		//sprite.setPosition(-sprite.getWidth()/2, -sprite.getHeight()/2);
 	}
 
 	@Override
@@ -46,9 +47,9 @@ public class RushHour implements ApplicationListener {
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
 		
-		batch.setProjectionMatrix(camera.combined);
+		//batch.setProjectionMatrix(camera.combined);
 		batch.begin();
-		sprite.draw(batch);
+		batch.draw(texture,0, 0, w, h,0, 0, 512, 256, false, false);
 		batch.end();
 	}
 
